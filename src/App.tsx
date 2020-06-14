c28ac6921785a73bfc779a428fde080fc3d2e04c
Vedant Koditkar <vedkoditkar@gmail.com>
2020-06-14 05:58:29 +0530

    update app to display all blogs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
+import BlogList from './containers/BlogList'
 import './App.css';
+import { allBlogs } from './util'
+
+allBlogs().then(x => console.log(x))
 
 function App() {
+  const [blogs, setBlogs] = useState<any | undefined>([])
+  useEffect(() => {
+    if (blogs.length === 0) {
+      allBlogs().then(setBlogs)
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: '760px',
+      }}
+    >
+      <header>
+        <ul>
+          <li>{'Blogs'}</li>
+          <li>{'About'}</li>
+        </ul>
       </header>
+      <main>
+        <BlogList blogs={blogs} />
+      </main>
+      <footer>
+        © {new Date().getFullYear()}, simplified.io
+      </footer>
     </div>
   );
 }
