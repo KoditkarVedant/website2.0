836dea0de30f7e9bedbe05ba92e534f94b9d1d2e
Vedant Koditkar <vedkoditkar@gmail.com>
2020-07-19 16:44:16 +0530

    add styled components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.header`
+    background-color: transparent;
+    min-height: 10vh;
+    color: #1f1f1f;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0 50px;
+    align-items: center;
+    z-index: 1;
+  
+    & > .App-header-nav {
+        list-style: none;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+    }
+    
+    & > .App-header-nav > li > a {
+        padding: .4rem .6rem;
+        font-size: 20px;
+        color: #f2f2f2;
+    }
+    
+    & > .App-header-nav > li > a:hover {
+        background: black;
+        border-radius: 9px;
+    }
+    
+    & > .App-header-name {
+        font-weight: 500;
+    }
+`
 
 const Header = () => {
     return (
-        <header className="App-header">
+        <HeaderWrapper>
             <h1 className="App-header-name">
                 <Link to="/">{'VEDANT'}</Link>
             </h1>
@@ -19,7 +54,7 @@ const Header = () => {
                     </Link>
                 </li>
             </ul>
-        </header>
+        </HeaderWrapper>
     )
 }
 
