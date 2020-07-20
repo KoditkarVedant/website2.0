46fc001bb03b43ee747991d4480bb78dc9d2c2d3
Vedant Koditkar <vedkoditkar@gmail.com>
2020-07-20 22:23:12 +0530

    clean up config-overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,9 @@
 const _ = require('lodash')
+const {
+    override,
+    addWebpackModuleRule
+} = require('customize-cra')
 
-module.exports = {
-    webpack: function (config, env) {
-        const rules = _.find(config.module.rules, x => x.oneOf)
-        const fileLoaderRuleIndex = _.findIndex(
-            rules.oneOf, x => _.includes(x.loader, 'file-loader')
-        )
-        rules.oneOf.splice(fileLoaderRuleIndex, 0, {
-            test: /\.md$/i,
-            use: [
-                {
-                    loader: 'raw-loader'
-                }
-            ]
-        })
-        console.log('index', fileLoaderRuleIndex)
-        return config
-    }
-}
+module.exports = override(
+    addWebpackModuleRule({ test: /\.md$/i, use: 'raw-loader' })
+)
