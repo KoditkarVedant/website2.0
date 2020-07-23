b43738c16eee232e371d3c61c37802327e289d47
Vedant Koditkar <vedkoditkar@gmail.com>
2020-07-23 21:43:26 +0530

    add style for image so it avoid overflow

--- src/containers/Blog/Blog.tsx
+++ src/containers/Blog/Blog.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { getBlog } from '../../util'
-import {IBlog} from '../../util/util.types'
+import { IBlog } from '../../util/util.types'
 import { useParams } from 'react-router-dom'
 import ReactMarkdown from 'react-markdown'
 import Prism from 'prismjs'
 import 'prismjs/components/prism-jsx';
 import 'prismjs/themes/prism-okaidia.css';
+import styled from 'styled-components'
+
+const BlogWrapper = styled.div`
+    img {
+        max-width: 100%
+    }
+`
 
 const Blog = () => {
     const [blog, setBlog] = useState<IBlog | undefined>()
@@ -28,7 +35,9 @@ const Blog = () => {
     }
 
     return (
-        <ReactMarkdown source={blog.data} />
+        <BlogWrapper>
+            <ReactMarkdown source={blog.data} />
+        </BlogWrapper>
     )
 }
 
