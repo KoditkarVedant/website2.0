bda70355a8b6f848ca3acbf714e171ea1bc6ec1c
Vedant Koditkar <vedkoditkar@gmail.com>
2020-07-26 17:11:48 +0530

    add prismjs babel plugin to load code styles

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,11 @@
 const {
     override,
-    addWebpackModuleRule
+    addWebpackModuleRule,
+    useBabelRc
 } = require('customize-cra')
 
 module.exports = override(
+    useBabelRc(),
     addWebpackModuleRule({ test: /\.md$/i, use: 'raw-loader' }),
     addWebpackModuleRule({ test: /\.(md|markdown)$/, use: 'markdown-image-loader' })
 )
